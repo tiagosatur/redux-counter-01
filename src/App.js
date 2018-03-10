e3c4613f2d8cef6a2be940dbf60b98455609d1e7
Tiago Satur <tiagosatur@gmail.com>
2018-03-10 15:05:07 -0300

    App configured up and runing with scss

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<h1 className='App__title'>Ritmo mexicano</h1>
+			</div>
+		);
+	}
 }
 
 export default App;
